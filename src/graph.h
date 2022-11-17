558251f4e0f42273ddb095c343efa502c2ebdecf
Marcel Hoang <62364538+marcelparcel@users.noreply.github.com>
2022-11-17 19:28:29 +0000

    changed distance formula comment

--- src/graph.h
+++ src/graph.h
@@ -38,6 +38,23 @@ class Graph {
     Graph(string filename);
 
     vector<Node*> DijkstraSP(Graph G, Node* s);
+
+    /**
+     * Finding the distance between two coordinates in radians using the haversine formula
+     * 
+     * degrees to radians: degrees * pi / 180
+     * 
+     * φ = latitude
+     * λ = longitude
+     * R = Earth's radius = 6,371km
+     * 
+     * Haversine Formula:
+     * a = sin²(Δφ/2) + cos φ1 ⋅ cos φ2 ⋅ sin²(Δλ/2)
+     * c = 2 ⋅ atan2( √a, √(1−a) )
+     * distance = R ⋅ c
+     * 
+     * @cite https://www.movable-type.co.uk/scripts/latlong.html
+    */
     double calculateDistance(Node* one, Node* two);
 
     private:
