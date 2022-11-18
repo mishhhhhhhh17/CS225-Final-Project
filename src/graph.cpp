e878ef732f179ab3ee0cfe53ba50f4ead0375424
Marcel Hoang <62364538+marcelparcel@users.noreply.github.com>
2022-11-18 00:28:32 +0000

    Initial Dijkstra implementation by Anna

--- src/graph.cpp
+++ src/graph.cpp
@@ -16,34 +16,37 @@ Graph::Graph(string filename) {
         data_.push_back(vec);
     }
 
+    Node* lowest;
     for(unsigned int i = 1; i < data_.size(); i++) {
-        Node* toInsert = new Node();
-        toInsert->incidentID = stoi(data_[i][0]);
-        toInsert->totalDead = stoi(data_[i][1]);
+        struct Node* toInsert = new Node();
+        toInsert->incidentID = data_[i][0];
+        toInsert->totalLoss = stoi(data_[i][1]);
         toInsert->totalMigrants = stoi(data_[i][2]);
         pair<double, double> coor;
         coor.first = stod(data_[i][3]);
         coor.second = stod(data_[i][4]);
         toInsert->coordinates = coor;
-        vertices.insert(toInsert);
+        if (i == 1) lowest == toInsert;
+        if (calculateRisk(toInsert) < calculateRisk(lowest)) lowest = toInsert;
+        vertices_.insert(toInsert);
     }
 
-    for (auto v : vertices) { // populating edgeList
+    for (auto v : vertices_) { // populating edgeList
         vector<pair<Node*, double>> vect;
-        for (auto e : vertices) {
+        for (auto e : vertices_) {
             if (calculateDistance(v, e) != 0) {
                 vect.push_back({e, calculateDistance(v, e)});
             }
         }
-        edgeList[v] = vect;
+        edgeList_[v] = vect;
     }
 }
 
-vector<Graph::Node*> Graph::DijkstraSP(Graph G, Node* s) {
-    // code here
-    vector<Node*> v;
-    return v;
+vector<Graph::Node*> Graph::getShortestPath(Node* node) {
+    return short_paths_[node];
 }
+
+double Graph::calculateRisk(Node* node) { return node->totalLoss / node->totalMigrants; }
 
 double Graph::calculateDistance(Node* one, Node* two){
     double distance = 0;
@@ -51,4 +54,37 @@ double Graph::calculateDistance(Node* one, Node* two){
     double yValueDiff = (one->coordinates.second - two->coordinates.second) * (one->coordinates.second - two->coordinates.second);
     distance = sqrt(xValueDiff+yValueDiff);
     return distance;
+}
+
+void Graph::dijkstra(Node* node) {
+    priority_queue<pair<double, Node*>> pq;
+ 
+    map<Node*, double> dist;
+ 
+     pq.push({0, node});
+     dist[node] = 0;
+
+    while (!pq.empty()) {
+        
+        Node* u = pq.top().second;
+        pq.pop();
+ 
+        std::vector<pair<Node*, double>>::iterator i;
+        for (i = edgeList_[u].begin(); i != edgeList_[u].end(); ++i) {
+            // Get vertex label and weight of current
+            // adjacent of u.
+            Node* v = (*i).first;
+            double weight = (*i).second;
+ 
+            // If there is shorted path to v through u.
+            if (dist[v] > dist[u] + weight) {
+                // Updating distance of v
+                dist[v] = dist[u] + weight;
+                pq.push({dist[v], v});
+                
+                // add vertex to path from source to final node
+                short_paths_[v].push_back(v);
+            }
+         }
+    }
 }
