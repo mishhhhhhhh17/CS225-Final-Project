d055dc7bfce1c9ba77e08c13b586dfef221137d2
Adriana Shuaipaj <92822481+ashua2@users.noreply.github.com>
2022-11-28 00:06:10 -0600

    Delete build directory

--- build/test_tests-b12d07c.cmake
+++
@@ -1,5 +0,0 @@
-add_test( [==[Data parsing from CSV]==] /root/cs225/CS225-Final-Project/build/test [==[Data parsing from CSV]==]  )
-set_tests_properties( [==[Data parsing from CSV]==] PROPERTIES WORKING_DIRECTORY /root/cs225/CS225-Final-Project/build)
-add_test( [==[Graph members]==] /root/cs225/CS225-Final-Project/build/test [==[Graph members]==]  )
-set_tests_properties( [==[Graph members]==] PROPERTIES WORKING_DIRECTORY /root/cs225/CS225-Final-Project/build)
-set( test_TESTS [==[Data parsing from CSV]==] [==[Graph members]==])
