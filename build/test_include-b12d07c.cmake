959340c2eacf5bcc3da087d162f5f5c94be0fc5f
Adriana Shuaipaj <ashua2@illinois.edu>
2022-11-28 06:03:28 +0000

    added test cases

---
+++ build/test_include-b12d07c.cmake
@@ -0,0 +1,5 @@
+if(EXISTS "/root/cs225/CS225-Final-Project/build/test_tests-b12d07c.cmake")
+  include("/root/cs225/CS225-Final-Project/build/test_tests-b12d07c.cmake")
+else()
+  add_test(test_NOT_BUILT-b12d07c test_NOT_BUILT-b12d07c)
+endif()
